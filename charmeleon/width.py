c8f2f51187d5c664544fac711632c9d91ef92b5d
yut-kt <yut.kt59@gmail.com>
2023-10-13 14:33:27 +0900

    update: width perfomance up

--- charmeleon/width.py
+++ charmeleon/width.py
@@ -20,36 +20,36 @@ class Width:
         if not any({digit, alpha, punct, kana}):
             msg = "At least one of digit, alpha, punct or kana must be True."
             raise ArgError(msg)
-        self.__h2f = {}
-        self.__f2h = {}
+        self.__h2f_table = {}
+        self.__f2h_table = {}
+        self.__hs2f_pattern = None
         if digit:
-            self.__h2f |= charmeleon.table.digit.HALF2FULL
-            self.__f2h |= charmeleon.table.digit.FULL2HALF
+            self.__h2f_table |= str.maketrans(charmeleon.table.digit.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.digit.FULL2HALF)
         if alpha:
-            self.__h2f |= charmeleon.table.alpha.HALF2FULL
-            self.__f2h |= charmeleon.table.alpha.FULL2HALF
+            self.__h2f_table |= str.maketrans(charmeleon.table.alpha.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.alpha.FULL2HALF)
         if punct:
-            self.__h2f |= charmeleon.table.punct.HALF2FULL
-            self.__f2h |= charmeleon.table.punct.FULL2HALF
+            self.__h2f_table |= str.maketrans(charmeleon.table.punct.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.punct.FULL2HALF)
         if kana:
-            self.__h2f |= charmeleon.table.kana.HALF2FULL
-            self.__f2h |= charmeleon.table.kana.FULL2HALF
-        self.__h2f_pattern = re.compile("|".join(map(re.escape, self.__h2f.keys())))
-        self.__f2h_pattern = re.compile("|".join(map(re.escape, self.__f2h.keys())))
-
+            self.__h2f_table |= str.maketrans(charmeleon.table.kana.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.kana.FULL2HALF)
+            self.__hs2f = charmeleon.table.kana.HALFS2FULL
+            self.__hs2f_pattern = re.compile(
+                "|".join(map(re.escape, self.__hs2f.keys())))
 
     def __h2f_replace(self: "Width", match: re.Match) -> str:
         """Replace function for re.sub."""
-        return self.__h2f[match.group(0)]
-
-    def __f2h_replace(self: "Width", match: re.Match) -> str:
-        """Replace function for re.sub."""
-        return self.__f2h[match.group(0)]
+        return self.__hs2f[match.group(0)]
 
     def to_full(self: "Width", chars: str) -> str:
         """Convert half-width characters to full-width characters."""
-        return self.__h2f_pattern.sub(self.__h2f_replace, chars)
+        if self.__hs2f_pattern:
+            return (self.__hs2f_pattern.sub(self.__h2f_replace, chars)
+                    .translate(self.__h2f_table))
+        return chars.translate(self.__h2f_table)
 
     def to_half(self: "Width", chars: str) -> str:
         """Convert full-width characters to half-width characters."""
-        return self.__f2h_pattern.sub(self.__f2h_replace, chars)
+        return chars.translate(self.__f2h_table)
