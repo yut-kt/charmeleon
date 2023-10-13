2b7b6676f773373329c4bc6019ee5ccd5da9cb39
Yuta Kato <32399978+yut-kt@users.noreply.github.com>
2023-10-13 18:10:42 +0900

    V0.1.1 (#29)
    * Width performance up (#27)
    
    * update: clean readme
    
    * update: kana table
    
    * update: width perfomance up
    
    * Add docs (#28)
    
    * add: install sphinx
    
    * add: sphinx doc
    
    * update: template is exclude

--- Makefile
+++ Makefile
@@ -3,9 +3,8 @@ install:
 	poetry insatll
 	pre-commit install
 
-.coverage:
+.PHONY: coverage
+coverage:
 	poetry run coverage run -m unittest
-
-coverage: .coverage
 	jq --arg percent "$$(coverage report --format=total)%" '.message = $$percent' asset/coverage.json > asset/coverage.json.tmp
 	mv asset/coverage.json.tmp asset/coverage.json
--- charmeleon/width.py
+++ charmeleon/width.py
@@ -16,40 +16,87 @@ class Width:
                  alpha: bool = True,
                  punct: bool = True,
                  kana: bool = True) -> None:
-        """Initialize Width class."""
+        """
+        Initialize Width class.
+
+        Args:
+        ----
+            digit (bool): True if digit characters are used.
+            alpha (bool): True if alphabet characters are used.
+            punct (bool): True if punctuation characters are used.
+            kana (bool): True if kana characters are used.
+
+        Raises:
+        ------
+            ArgError: If all of digit, alpha, punct and kana are False.
+        """
         if not any({digit, alpha, punct, kana}):
             msg = "At least one of digit, alpha, punct or kana must be True."
             raise ArgError(msg)
-        self.__h2f = {}
-        self.__f2h = {}
+        self.__h2f_table = {}
+        self.__f2h_table = {}
+        self.__hs2f_pattern = None
         if digit:
-            self.__h2f |= charmeleon.table.digit.HALF2FULL
-            self.__f2h |= charmeleon.table.digit.FULL2HALF
+            self.__h2f_table |= str.maketrans(charmeleon.table.digit.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.digit.FULL2HALF)
         if alpha:
-            self.__h2f |= charmeleon.table.alpha.HALF2FULL
-            self.__f2h |= charmeleon.table.alpha.FULL2HALF
+            self.__h2f_table |= str.maketrans(charmeleon.table.alpha.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.alpha.FULL2HALF)
         if punct:
-            self.__h2f |= charmeleon.table.punct.HALF2FULL
-            self.__f2h |= charmeleon.table.punct.FULL2HALF
+            self.__h2f_table |= str.maketrans(charmeleon.table.punct.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.punct.FULL2HALF)
         if kana:
-            self.__h2f |= charmeleon.table.kana.HALF2FULL
-            self.__f2h |= charmeleon.table.kana.FULL2HALF
-        self.__h2f_pattern = re.compile("|".join(map(re.escape, self.__h2f.keys())))
-        self.__f2h_pattern = re.compile("|".join(map(re.escape, self.__f2h.keys())))
-
+            self.__h2f_table |= str.maketrans(charmeleon.table.kana.HALF2FULL)
+            self.__f2h_table |= str.maketrans(charmeleon.table.kana.FULL2HALF)
+            self.__hs2f = charmeleon.table.kana.HALFS2FULL
+            self.__hs2f_pattern = re.compile(
+                "|".join(map(re.escape, self.__hs2f.keys())))
 
     def __h2f_replace(self: "Width", match: re.Match) -> str:
         """Replace function for re.sub."""
-        return self.__h2f[match.group(0)]
-
-    def __f2h_replace(self: "Width", match: re.Match) -> str:
-        """Replace function for re.sub."""
-        return self.__f2h[match.group(0)]
+        return self.__hs2f[match.group(0)]
 
     def to_full(self: "Width", chars: str) -> str:
-        """Convert half-width characters to full-width characters."""
-        return self.__h2f_pattern.sub(self.__h2f_replace, chars)
+        """
+        Convert half-width characters to full-width characters.
+
+        Args:
+        ----
+            chars (str): Characters to be converted.
+
+        Returns:
+        -------
+            str: Converted characters.
+
+        Examples:
+        --------
+            >>> from charmeleon import Width
+            >>> width = Width()
+            >>> width.to_full("abc")
+            'ａｂｃ'
+        """
+        if self.__hs2f_pattern:
+            return (self.__hs2f_pattern.sub(self.__h2f_replace, chars)
+                    .translate(self.__h2f_table))
+        return chars.translate(self.__h2f_table)
 
     def to_half(self: "Width", chars: str) -> str:
-        """Convert full-width characters to half-width characters."""
-        return self.__f2h_pattern.sub(self.__f2h_replace, chars)
+        """
+        Convert full-width characters to half-width characters.
+
+        Args:
+        ----
+            chars (str): Characters to be converted.
+
+        Returns:
+        -------
+            str: Converted characters.
+
+        Examples:
+        --------
+            >>> from charmeleon import Width
+            >>> width = Width()
+            >>> width.to_half("ＡＢＣ")
+            'ABC'
+        """
+        return chars.translate(self.__f2h_table)
